4bd254de38798970bef89e39464510340c039dd5
Jingfei Cai-Pincus <fay.cai.jingfei@gmail.com>
2020-12-03 22:31:39 -0500

    simplify day 3 solution

--- 2020/day3.py
+++ 2020/day3.py
@@ -6,42 +6,30 @@ from operator import mul
 
 from read_input import *
 
+
 input_string = get_input_string(day)
 # input_string = read_file('test.txt')
 
+input_list = input_string.splitlines()
 
 # Part One
-def read_map(input_string):
-    return {
-        (x, y): symbol
-        for y, line in enumerate(input_string.splitlines())
-        for x, symbol in enumerate(line)
-    }
-
-
-def map_dimension(input_string):
-    length = input_string.rstrip('\n').count('\n') + 1
-    width = len(input_string.split('\n')[0])
-    return width, length
-
-
-def part1(input_string, x_rule=3, y_rule=1):
-    x_max, y_max = map_dimension(input_string)
-    tree_map = read_map(input_string)
-    tree_count = 0
+def part1(input_list, x_rule=3, y_rule=1):
+    y_max = len(input_list)
+    x_max = len(input_list[0])
+    hits = 0
     x, y = 0, 0
     while y < y_max:
-        if tree_map[(x % x_max, y)] == '#':
-            tree_count += 1
+        if input_list[y][x % x_max] == '#':
+            hits += 1
         x += x_rule
         y += y_rule
-    return tree_count
+    return hits
 
 
 # Part Two
-def part2(input_string):
+def part2(input_list):
     return reduce(mul,
-                  (part1(input_string, x_rule, y_rule)
+                  (part1(input_list, x_rule, y_rule)
                    for x_rule, y_rule in (
                        (1, 1),
                        (3, 1),
@@ -53,6 +41,6 @@ def part2(input_string):
 
 
 if __name__ == '__main__':
-    print(part1(input_string))
-    print(part2(input_string))
+    print(part1(input_list))
+    print(part2(input_list))
 
